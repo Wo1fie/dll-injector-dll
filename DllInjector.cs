68e04f94487754265677c9efe3f57409c5403141
Wo1fie <wo1fie@live.com>
2023-10-23 19:14:09 -0700

    Initial Commit

---
+++ DllInjector.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace dll_injector
+{
+    public class DllInjector
+    {
+        #region Function Imports
+        [DllImport("kernel32")]
+        static extern int GetLastError();
+
+        [DllImport("kernel32", SetLastError = true, ExactSpelling = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32", SetLastError = true, ExactSpelling = true)]
+        private static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint dwFreeType);
+
+        [DllImport("kernel32", SetLastError = true, ExactSpelling = true)]
+        internal static extern int WaitForSingleObject(IntPtr handle, int milliseconds);
+
+        [DllImport("kernel32")]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, string lpBuffer, UIntPtr nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, UIntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out IntPtr lpThreadId);
+
+        [DllImport("kernel32", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true)]
+        public static extern UIntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32")]
+        static extern bool CloseHandle(IntPtr hObject);
+        #endregion
+        public enum InjectReturnStatus { Success, OpenProcess, VirtualAllocEx, WriteProcessMemory, GetProcAddress, CreateRemoteThread, CloseHandle, VirtualFreeEx };
+
+        Process _process;
+        public DllInjector(Process process)
+        {
+            _process = process;
+        }
+        public InjectReturnStatus InjectDll(FileInfo dllFile)
+        {
+            return InjectDll(_process, dllFile);
+        }
+        public static InjectReturnStatus InjectDll(Process process, FileInfo dllFile)
+        {
+            IntPtr hProcess = OpenProcess(0x1f0fff, 1, process.Id);
+            IntPtr ptr;
+            int dllPathLength = dllFile.FullName.Length + 1;
+            IntPtr lpBaseAddress = VirtualAllocEx(hProcess, IntPtr.Zero, (uint)dllPathLength, 0x1000, 0x40);
+            if (lpBaseAddress == IntPtr.Zero)
+                return InjectReturnStatus.VirtualAllocEx;
+            bool rawr = WriteProcessMemory(hProcess, lpBaseAddress, dllFile.FullName, (UIntPtr)dllPathLength, out ptr);
+            if (!rawr)
+                return InjectReturnStatus.WriteProcessMemory;
+            UIntPtr procAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            if (procAddress == UIntPtr.Zero)
+                return InjectReturnStatus.GetProcAddress;
+            IntPtr handle = CreateRemoteThread(hProcess, IntPtr.Zero, 0, procAddress, lpBaseAddress, 0, out ptr);
+            if (handle == IntPtr.Zero)
+                return InjectReturnStatus.CreateRemoteThread;
+
+            switch ((long)WaitForSingleObject(handle, 0x2710))
+            {
+                case 0x80L:
+                case 0x102L:
+                case 0xffffffffL:
+                    if (!CloseHandle(handle))
+                        return InjectReturnStatus.CloseHandle;
+                    break;
+            }
+            if (!VirtualFreeEx(hProcess, lpBaseAddress, (UIntPtr)0, 0x8000))
+                return InjectReturnStatus.VirtualFreeEx;
+
+            if (!CloseHandle(handle))
+                return InjectReturnStatus.CloseHandle;
+            return InjectReturnStatus.Success;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dll-injector-dll")]
+[assembly: AssemblyDescription("Simple C# Dll Injector remade in 2023.  Originally made in 2009.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ryan Blanco")]
+[assembly: AssemblyProduct("dll-injector-dll")]
+[assembly: AssemblyCopyright("Copyright © Ryan Blanco 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39efcfbd-c8f1-488b-9237-1ec04d33d8d6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
